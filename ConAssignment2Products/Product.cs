3ab25f3a6dd8526c07aab3cc8e73b01fadeb3d06
sahaja <sahajaboyapati@gmail.com>
2023-11-21 16:53:04 +0530

    intial commit

---
+++ ConAssignment2Products/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAssignment2Products
+{
+
+    public class Product
+    {
+        int pid;
+        string pname;
+        double pprice;
+        public Product()
+        {
+            Console.WriteLine("Default Constructor");
+            pid = 0;
+            pname = "no name";
+            pprice = 0;
+        }
+
+        public virtual void Set()
+        {
+            Console.WriteLine("Enter Product Id");
+            pid = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Product Name");
+            pname = Console.ReadLine();
+            Console.WriteLine("Enter Product Price");
+            pprice = double.Parse(Console.ReadLine());
+        }
+
+        public virtual void Show()
+        {
+            Console.WriteLine("ID: " + pid);
+            Console.WriteLine("Name: " + pname);
+            Console.WriteLine("Price: " + pprice);
+        }
+    }
+}
---
+++ ConAssignment2Products/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAssignment2Products
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HerbalProduct product = new HerbalProduct();
+            product.Set();
+            product.Show();
+            Console.ReadKey();
+        }
+    }
+}
